b537f867dd4c6ff0663ebe12071637bd08847f2b
Lucas Van Skyhawk <luke@vanskyhawk.com>
2018-07-13 12:42:26 -0600

    latest version added clock, picker, button components and styled them. ready to import to actual mac

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import Picker from './picker';
+import Button from './button';
+import Clock from './clock';
+import ChangeDate from './changeDate';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: false
+    }
+  }
+
+  renderItems = function () {
+    if (this.state.active) {
+      return [
+        <Clock />,
+        ChangeDate('Change Date', () => this.setState({ active: false }))
+      ]
+    } else {
+      return Button('Generate Countdown', () => this.setState({ active: true }))
+    }
+  }.bind(this)
   render() {
     return (
-      <div>
-        <h1>Birthday Countdown</h1>
+      <div className="grid">
+        <h1 className="grid__title">Birthday Countdown</h1>
+
+        <div className="grid__skew-dark-one-box"></div>
+        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
+
+        <div className="grid__skew-light-one"></div>
+        <div className="grid__skew-light-two"></div>
+        <div className="grid__skew-light-three-box"></div>
+
+        <Picker />
+        {this.renderItems()}
       </div>
     );
   }
